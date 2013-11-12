40ee5e5a296efaafe0b162dd8b94d30100c9b3ee
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-12 16:44:16 +0000

    UPloading v1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/love-it/trunk@803194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/display-functions.php
+++ includes/display-functions.php
@@ -6,7 +6,7 @@ function li_love_link($love_text = null, $loved_text = null) {
 	global $user_ID, $post;
 
 	// only show the link when user is logged in and on a singular page
-	if(is_user_logged_in()) {
+	if( is_user_logged_in() ) {
 
 		ob_start();
 	
@@ -16,11 +16,11 @@ function li_love_link($love_text = null, $loved_text = null) {
 		// our wrapper DIV
 		echo '<div class="love-it-wrapper">';
 		
-			$love_text = is_null($love_text) ? __('Love It', 'love_it') : $love_text;
-			$loved_text = is_null($loved_text) ? __('You have loved this', 'love_it') : $loved_text;
+			$love_text = is_null($love_text) ? __( 'Love It', 'love_it' ) : $love_text;
+			$loved_text = is_null($loved_text) ? __( 'You have loved this', 'love_it' ) : $loved_text;
 			
 			// only show the Love It link if the user has NOT previously loved this item
-			if(!li_user_has_loved_post($user_ID, get_the_ID())) {
+			if( ! li_user_has_loved_post( $user_ID, get_the_ID() ) ) {
 				echo '<a href="#" class="love-it" data-post-id="' . get_the_ID() . '" data-user-id="' .  $user_ID . '">' . $love_text . '</a> (<span class="love-count">' . $love_count . '</span>)';
 			} else {
 				// show a message to users who have already loved this item
@@ -37,10 +37,13 @@ function li_love_link($love_text = null, $loved_text = null) {
 }
 
 // adds the Love It link and count to post/page content automatically
-function li_display_love_link($content) {
-	if(is_singular() && is_user_logged_in()) {
+function li_display_love_link( $content ) {
+
+	$types = apply_filters( 'li_display_love_links_on', array( 'post' ) );
+
+	if( is_singular( $types ) && is_user_logged_in() ) {
 		$content .= li_love_link();
 	}
 	return $content;
 }
-add_filter('the_content', 'li_display_love_link', 100);
+add_filter( 'the_content', 'li_display_love_link', 100 );
